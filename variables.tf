83e04a87cf281946c0acaa96576e9e26d218c254
Shinichi Urano <s@urano.me>
2024-07-23 11:58:34 -0400

    format fix

--- variables.tf
+++ variables.tf
@@ -1,22 +1,22 @@
 variable "common_name" {
   description = "DNS Common Name for the SSL Cert"
-  type = string
+  type        = string
 }
 
 variable "dns_alt_names" {
   description = "DNS Alt Names for the SSL Cert"
-  type    = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 variable "email" {
   description = "The Email to associate with the SSL Cert"
-  type = string
+  type        = string
 }
 
 variable "s3_bucket_name" {
   description = "S3 bucket that will store the SSL Private Key and Certificate"
-  type = string
+  type        = string
 }
 
 variable "dns_challenge_route53_config" {
